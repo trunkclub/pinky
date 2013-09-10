bb129e3564eddc4e9828601b75e23f84ac0b459a
Dean Strelau <dean@trunkclub.com>
2013-09-10 16:46:17 -0500

    Remove old unused requires.

--- lib/pinky.rb
+++ lib/pinky.rb
@@ -1,8 +1,5 @@
 require 'json'
 require 'httparty'
 require 'date'
-require 'uuid'
-
-require 'hot_bunnies'
 
 Dir[File.join(File.dirname(__FILE__), 'pinky', '**', '*.rb')].sort.each { |f| require f }
